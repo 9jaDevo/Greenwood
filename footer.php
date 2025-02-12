c12fa71759b64e08e2a848fdeae39f8cfdfe40e7
9jaDevO <macruben03@gmail.com>
2025-02-12 20:31:17 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,35 +1,35 @@
 <!-- jQuery CDN -->
-<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src="assets/js/jquery-3.7.1.min.js"></script>
 
 <!-- Bootstrap Bundle CDN -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+<script src="assets/js/bootstrap.bundle.min.js"></script>
 
 <!-- Swiper CDN -->
-<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+<script src="assets/js/swiper.min.js"></script>
 
 <!-- WOW.js CDN -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
+<script src="assets/js/wow.min.js"></script>
 
 <!-- Appear.js CDN -->
-<script src="https://cdn.jsdelivr.net/npm/jquery-appear@0.4.1/jquery.appear.min.js"></script>
+<script src="assets/js/appear.js"></script>
 
 <!-- Odometer CDN -->
-<script src="https://cdn.jsdelivr.net/npm/odometer@0.4.9/odometer.min.js"></script>
+<script src="assets/js/odometer.min.js"></script>
 
 <!-- Magnific Popup CDN -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-magnific-popup/1.1.0/jquery.magnific-popup.min.js"></script>
+<script src="assets/js/jquery.magnific-popup.min.js"></script>
 
 <!-- Easing.js CDN -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+<script src="assets/js/easing.min.js"></script>
 
 <!-- ScrollSpy.js CDN (if needed) -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/scrollspy/1.0.0/scrollspy.min.js"></script>
+<script src="assets/js/scrollspy.js"></script>
 
 <!-- Countdown.js CDN -->
-<script src="https://cdn.jsdelivr.net/npm/countdown@2.6.0/dist/countdown.min.js"></script>
+<script src="assets/js/countdown.js"></script>
 
 <!-- Parallax Scroll CDN -->
-<script src="https://cdn.jsdelivr.net/npm/parallax-scroll@1.0.2/parallax-scroll.min.js"></script>
+<script src="assets/js/parallax-scroll.js"></script>
 
 <!-- Main.js -->
 <script src="assets/js/main.js"></script>
