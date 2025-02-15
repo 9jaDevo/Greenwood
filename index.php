4e9f206bd6db1365bdc3d8bcd95fc29915b12d47
9jaDevO <macruben03@gmail.com>
2025-02-15 09:41:21 +0100

    Update index.php

--- index.php
+++ index.php
@@ -172,7 +172,8 @@ include('header.php');
                             </a>
                         </div>
                         <div class="hero-dashbord wow fadeInUp" data-wow-duration=".7s" data-wow-delay="350ms">
-                            <img src="assets/img/hero/Dashbord.png" alt="" class="d-none d-lg-flex">
+                            <img src="assets/img/hero/Dashbord.png" alt="" class="d-none d-sm-block">
+
                             <div class="dashbord-shape">
                                 <div class="shape shape--1">
                                     <img class="topToBottom" src="assets/img/shape/hero-sp_09.svg" alt="">
