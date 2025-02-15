bb0b7cd7f7de9db9c8bc598fb0e80207c0fa2486
9jaDevO <macruben03@gmail.com>
2025-02-15 10:00:42 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,10 +1,36 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="description" content="">
+    <meta name="description" content="GreenWoodAI is a platform offering airdrops and presales. Join now to get early access to the latest AI-based token opportunities.">
+    <meta name="keywords" content="GreenWoodAI, Airdrop, Presales, AI, Token, Cryptocurrency">
+    <meta name="author" content="GreenWoodAI Team">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>GreenWordAI - Airdrop and Presales</title>
-    <link rel="shortcut icon" href="assets/img/favicon.png" type="images/x-icon" />
+
+    <!-- Title for Search Engines -->
+    <title>GreenWoodAI - Airdrop and Presales</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="assets/img/favicon.png" type="image/x-icon" />
+
+    <!-- Open Graph Meta Tags for Social Media (Facebook, LinkedIn, etc.) -->
+    <meta property="og:title" content="GreenWoodAI - Airdrop and Presales">
+    <meta property="og:description" content="GreenWoodAI is a platform offering airdrops and presales. Join now to get early access to the latest AI-based token opportunities.">
+    <meta property="og:image" content="assets/img/about/about-img.png">
+    <meta property="og:url" content="https://airdrop.greenwoodai.space/">
+    <meta property="og:type" content="website">
+    <meta property="og:site_name" content="GreenWoodAI">
+    <meta property="og:locale" content="en_US">
+
+    <!-- Twitter Card Meta Tags for Social Media (Twitter) -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@GreenWoodAI">
+    <meta name="twitter:title" content="GreenWoodAI - Airdrop and Presales">
+    <meta name="twitter:description" content="GreenWoodAI is a platform offering airdrops and presales. Join now to get early access to the latest AI-based token opportunities.">
+    <meta name="twitter:image" content="assets/img/about/about-img.png">
+    <meta name="twitter:url" content="https://airdrop.greenwoodai.space/">
+
+    <!-- Open Graph and Twitter Image Size Recommendations -->
+    <!-- For OG and Twitter, use at least 1200x630 pixels for images -->
 
     <!-- CSS includes -->
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
